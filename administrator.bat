09828a57fdeec4bc0f2a8eba13d845da2ffa8238
zhoushanbao1994 <zhoushanbao@126.com>
2020-06-06 12:03:18 +0800

    自动以管理员运行bat

---
+++ administrator.bat
@@ -0,0 +1,18 @@
+:: 自动以管理员身份运行
+@echo off
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+
+:: 添加处理语句
+echo "hello"
+
+pause >NUL && EXIT
