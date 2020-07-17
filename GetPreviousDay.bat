936ba68ada62d09d2bb5a9635f43c7ad8ec0d233
zhoushanbao1994 <zhoushanbao@126.com>
2020-07-17 14:42:10 +0800

    获取前一天、一月、一年的日期

---
+++ GetPreviousDay.bat
@@ -0,0 +1,21 @@
+@echo off & setlocal ENABLEEXTENSIONS
+
+:: 利用Powershell提供的函数，批处理文件获取前一天的日期
+for /f "delims=" %%a in ( 'PowerShell  "&{Get-Date (Get-Date).AddDays(-1) -uformat "%%Y%%m%%d"}"' ) do (
+	set date1=%%a
+)
+echo %date1%
+
+:: 利用Powershell提供的函数，批处理文件获取前一月的日期
+for /f "delims=" %%a in ( 'PowerShell  "&{Get-Date (Get-Date).AddMonths(-1) -uformat "%%Y%%m%%d"}"' ) do (
+	set date2=%%a
+)
+echo %date2%
+
+:: 利用Powershell提供的函数，批处理文件获取前一年的日期
+for /f "delims=" %%a in ( 'PowerShell  "&{Get-Date (Get-Date).AddYears(-1) -uformat "%%Y%%m%%d"}"' ) do (
+	set date3=%%a
+)
+echo %date3%
+
+pause
