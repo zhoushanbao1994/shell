acb8a0b6c9d6ba1b2ed8fc406b127ebd535c64bb
zhoushanbao1994 <zhoushanbao@126.com>
2020-07-17 14:54:14 +0800

    增加指定日期

--- GetPreviousDay.bat
+++ GetPreviousDay.bat
@@ -18,4 +18,10 @@ for /f "delims=" %%a in ( 'PowerShell  "&{Get-Date (Get-Date).AddYears(-1) -ufor
 )
 echo %date3%
 
+:: 利用Powershell提供的函数，批处理文件获取指定的日期
+for /f "delims=" %%a in ( 'PowerShell  "&{Get-Date (get-date -year 2000 -month 12 -day 31) -uformat "%%Y%%m%%d"}"' ) do (
+	set date4=%%a
+)
+echo %date4%
+
 pause
